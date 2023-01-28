dc3707b4517e776090addf40ce6e66d43b8459ea
haden <80521474+thdefn@users.noreply.github.com>
2023-01-28 09:33:43 +0900

    array

---
+++ src/ObjectOriented/모스부호테스트.java
@@ -0,0 +1,28 @@
+package ObjectOriented;
+
+import java.util.Scanner;
+
+public class 모스부호테스트 {
+    public static void main(String[] args) {
+        String[] morseCode={
+                ".-","-...","-.-.","-..",".","..-.","--.","....","..",
+                ".---","-.-",".-..","--","-.","---",".--.","--.-",".-.",
+                "...","-","..-","...-",".--","-..-","-.--","--..", "-----",
+                ".----","..---","...--","....-",".....","-....","--...","---..","----."
+        };
+        Scanner scanner = new Scanner(System.in);
+        String str = scanner.next();
+        int ascii = 0;
+        for(int i=0;i<str.length();i++){
+            ascii = (int) str.charAt(i); //char을 int로 바꿔주면 ascii 값
+            if(ascii>=65 && ascii<=90){
+                ascii = ascii - 65;
+            } else if (ascii>=97 && ascii<=122) {
+                ascii = ascii - 97;
+            } else if(ascii>=48 && ascii<=57){
+                ascii = ascii - 22;
+            }
+            System.out.println(morseCode[ascii]);
+        }
+    }
+}
---
+++ src/ObjectOriented/배열초기화테스트.java
@@ -0,0 +1,58 @@
+package ObjectOriented;
+/*
+배열
+-2차원 배열
+-3차원 배열
+-다차원 배열
+
++
+업무에서는 대부분 제네릭 클래스를 사용하기 때문에 배열을 자주 활용하지 않음
+* */
+public class 배열초기화테스트 {
+    public static void main(String[] args) {
+        int[] arr1 = {
+                1,
+                2,
+                3
+        };
+
+        int[][] arr2 = {
+                {1,2,3},
+                {1,2,3},
+                {1,2,3}
+        };
+
+        int[][][] arr3= {
+            {
+                    {1,2,3},
+                    {1,2,3},
+                    {1,2,3}
+            },
+            {
+                    {1,2,3},
+                    {1,2,3},
+                    {1,2,3}
+            },
+            {
+                    {1,2,3},
+                    {1,2,3},
+                    {1,2,3}
+            }
+        };
+
+
+        String[][] str = {
+                {"a1","b1","c1"},
+                {"a2","b2","c2"},
+                {"a3","b3","c3"}
+        };
+
+        for (int i = 0; i < str.length; i++) {
+            for (int j = 0; j <str[i].length ; j++) {
+                System.out.print(str[i][j]);
+            }
+            System.out.println();
+        }
+
+    }
+}

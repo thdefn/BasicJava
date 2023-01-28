b6669f4fb2410bd7ddaf515074d77ec778e26e1c
haden <80521474+thdefn@users.noreply.github.com>
2023-01-28 21:50:32 +0900

    extends and constructor

--- src/ObjectOriented/Usage/차량.java
+++ src/ObjectOriented/Usage/차량.java
@@ -5,6 +5,16 @@ public class 차량 {
     public double 최대속도;
     public int 정원;
 
+    public 차량(){
+        System.out.println("차량의 생성자 호출");
+    }
+
+
+    public 차량(double 최대속도, int 정원){
+        this.최대속도 = 최대속도;
+        this.정원 = 정원;
+        System.out.println("차량의 생성자(매개변수 : 최대속도, 정원) 호출");
+    }
     public void 정보출력(){
         System.out.println("이름: "+이름);
         System.out.println("최대속도: "+최대속도);

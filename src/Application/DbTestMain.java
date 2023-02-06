be77de07ff7f99bc0c355034b2d7afb968017eaa
haden <80521474+thdefn@users.noreply.github.com>
2023-02-06 10:40:36 +0900

    jdbc connect - insert

---
+++ src/Application/DbTestMain.java
@@ -0,0 +1,9 @@
+package Application;
+
+public class DbTestMain {
+    public static void main(String[] args) {
+        DbTest dbTest = new DbTest();
+        dbTest.dbSelect();
+        dbTest.dbInsert();
+    }
+}
